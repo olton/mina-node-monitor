edefa495a031f14e26615b00659b5a66d9539ee4
Sergey Pimenov <sergey@pimenov.com.ua>
2021-06-04 10:27:11 +0300

    improve server:alerter

--- server/alerter.mjs
+++ server/alerter.mjs
@@ -79,7 +79,7 @@ export const processAlerter = async (config) => {
 
             if (DIFF_MAX >= blockDiff) {
                 OK_MAX = false
-                message = `Difference block height detected! Height ${DIFF_MAX > 0 ? 'less' : 'more'} than max block length!\nDifference: ${DIFF_MAX} ${sign}`
+                message = `Difference block height detected! Height ${DIFF_MAX > 0 ? 'less' : 'more'} than max block length!\nDifference: ${Math.abs(DIFF_MAX)}\nNode: ${nHeight}\nMax: ${mHeight} ${sign}`
                 await telegram(message, {token: telegramToken, recipients: telegramChatIDAlert})
 
                 if (restartStateSyncedRules.includes("MAX")) {
@@ -96,7 +96,7 @@ export const processAlerter = async (config) => {
 
             if (DIFF_UNVALIDATED >= blockDiff) {
                 OK_UNV = false
-                message = `Difference block height detected! Height ${DIFF_UNVALIDATED > 0 ? 'less' : 'more'} than unvalidated block length!\nDifference: ${DIFF_UNVALIDATED} ${sign}`
+                message = `Difference block height detected! Height ${DIFF_UNVALIDATED > 0 ? 'less' : 'more'} than unvalidated block length!\nDifference: ${Math.abs(DIFF_UNVALIDATED)}\nNode: ${nHeight}\nUnvalidated: ${uHeight} ${sign}`
                 await telegram(message, {token: telegramToken, recipients: telegramChatIDAlert})
 
                 if (restartStateSyncedRules.includes("UNV")) {
@@ -149,14 +149,14 @@ export const processAlerter = async (config) => {
         if (OK_PREV) globalThis.restartTimerPrev = 0
         if (OK_SYNCED) globalThis.restartTimerNotSynced = 0
 
-        if (globalThis.currentHeight > 0 && observeExplorer) {
+        if (SYNCED && globalThis.currentHeight > 0 && observeExplorer) {
             const explorer = await getExplorerSummary()
             if (explorer && explorer.blockchainLength && blockchainLength) {
                 const DIFF_EXPLORER = +globalThis.currentHeight - explorer.blockchainLength
                 let message
                 if (Math.abs(DIFF_EXPLORER) >= blockDiff) {
                     message = DIFF_EXPLORER < 0 ? `Node lags behind the Explorer in block height.` : `Node leads the Explorer by block height.`
-                    message += `\nDifference: ${DIFF_EXPLORER}\nNode: ${globalThis.currentHeight}\nExplorer: ${explorer.blockchainLength}`
+                    message += `\nDifference: ${DIFF_EXPLORER}\nNode: ${globalThis.currentHeight}\nExplorer: ${explorer.blockchainLength}${sign}`
                     await telegram(message, {token: telegramToken, recipients: telegramChatIDAlert})
                 }
             }
