ad6cf0a053072b3fb1a0eaaa54fafb7dbaf6c1e1
Sergey Pimenov <sergey@pimenov.com.ua>
2021-06-24 18:51:45 +0300

    added color indicator for price change

--- client/js/coingecko.js
+++ client/js/coingecko.js
@@ -8,23 +8,33 @@ export const processCoingecko = async () => {
     const {currency = 'usd', interval = 60000} = globalThis.config.price
     const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=%CURRENCY%&ids=mina-protocol`.replace("%CURRENCY%", currency.toLowerCase())
 
+    const elCurrentPrice = $("#current-price")
+    const elCurrency = $("#currency")
+    const elPriceChange = $("#price-change")
+    const elPriceHigh = $("#price-high")
+    const elPriceLow = $("#price-low")
+    const elBalance = $("#balance-usd")
+
     const data = await getAPIData(url)
 
     if (data) {
         const mina = data[0]
         const sign = mina.price_change_percentage_24h
-        const symbol = `<span class="mif-${sign === 0 ? '' : sign <= 0 ? 'arrow-down fg-red' : 'arrow-up fg-green'}"></span>`
+        const symbol = `<span class="mif-${sign === 0 ? '' : sign <= 0 ? 'arrow-down' : 'arrow-up'}"></span>`
+        const changeColor = sign === 0 ? '' : sign <= 0 ? 'alert' : 'success'
 
         globalThis.price = +mina.current_price
 
-        $("#current-price").html(mina.current_price)
-        $("#currency").html(currency.toUpperCase())
-        $("#price-change").html(symbol + ' ' + +(mina.price_change_percentage_24h).toFixed(2))
-        $("#price-high").html(mina.high_24h)
-        $("#price-low").html(mina.low_24h)
+        elCurrentPrice.html(mina.current_price)
+        elCurrency.html(currency.toUpperCase())
+        elPriceChange.html(symbol + ' ' + +(mina.price_change_percentage_24h).toFixed(2))
+        elPriceHigh.html(mina.high_24h)
+        elPriceLow.html(mina.low_24h)
+
+        elPriceChange.closest(".panel").removeClass('alert success').addClass(changeColor)
 
 
-        $("#balance-usd").text((globalThis.balance * globalThis.price).format(2, null, ",", ".") + " " + currency.toUpperCase())
+        elBalance.text((globalThis.balance * globalThis.price).format(2, null, ",", ".") + " " + currency.toUpperCase())
 
         elLog.html(imgOk)
     }
