a19994a8aba60bc5c34a2873c1a7a98d9d94a0fd
Sergey Pimenov <sergey@pimenov.com.ua>
2021-06-24 10:21:43 +0300

    Client: added config check. If config contains errors, Monitor puts to the console error message

--- client/js/app.js
+++ client/js/app.js
@@ -15,75 +15,75 @@ const version = `1.0.5`
 
 $("#version").text(version)
 
-fetch("./config.json").then( (r) => r.ok ? r.text() : null ).then(response => {
-    const html = $("html")
-    const body = $("body")
-    let config
+fetch("./config.json")
+    .then(r => {
+        if (!r.ok) {
+            throw new Error(`Config is wrong or not loaded!`)
+        }
+        return r.json()
+    })
+    .then( config => {
+        globalThis.config = config
 
-    console.info(`Mina Monitor v${version}`)
-
-    try {
-        config = JSON.parse(response)
         console.info(`Mina Monitor Client config load successfully!`)
-    } catch (e) {
-        console.error("Config is wring or contains errors!")
-        console.error(e.message)
-    }
 
-    globalThis.config = config
+        const html = $("html")
+        const body = $("body")
 
-    if (config.theme === "auto") {
-        globalThis.darkMode = $.dark
-        if (globalThis.darkMode) {
-            html.removeClass("light-mode")
-            body.removeClass("light-mode")
+        if (config.theme === "auto") {
+            globalThis.darkMode = $.dark
+            if (globalThis.darkMode) {
+                html.removeClass("light-mode")
+                body.removeClass("light-mode")
+            } else {
+                html.addClass("light-mode")
+                body.addClass("light-mode")
+            }
         } else {
-            html.addClass("light-mode")
-            body.addClass("light-mode")
+            globalThis.darkMode = config.theme !== "light"
+            if (config.theme === "light") {
+                html.addClass("light-mode")
+                body.addClass("light-mode")
+            }
         }
-    } else {
-        globalThis.darkMode = config.theme !== "light"
-        if (config.theme === "light") {
-            html.addClass("light-mode")
-            body.addClass("light-mode")
+
+        globalThis.chartLineColor = globalThis.darkMode ? '#3c424b' : "#e5e5e5"
+        globalThis.chartLabelColor = globalThis.darkMode ? "#fff" : "#000"
+        globalThis.balance = 0
+        globalThis.price = 0
+        globalThis.blockSpeed = 0
+        globalThis.blockHeight = 0
+        globalThis.epoch = 0
+        globalThis.noSlots = false
+        globalThis.cpuCores = 0
+
+        if (config.blocks) {
+            const container = $("#main-section-container")
+
+            container.children().hide()
+
+            config.blocks.forEach(b => {
+                container.append($(`#${b}-group`).show())
+            })
         }
-    }
 
-    globalThis.chartLineColor = globalThis.darkMode ? '#3c424b' : "#e5e5e5"
-    globalThis.chartLabelColor = globalThis.darkMode ? "#fff" : "#000"
-    globalThis.balance = 0
-    globalThis.price = 0
-    globalThis.blockSpeed = 0
-    globalThis.blockHeight = 0
-    globalThis.epoch = 0
-    globalThis.noSlots = false
-    globalThis.cpuCores = 0
-
-    if (config.blocks) {
-        const container = $("#main-section-container")
-
-        container.children().hide()
-
-        config.blocks.forEach(b => {
-            container.append($(`#${b}-group`).show())
-        })
-    }
-
-    setTimeout(() => processCoingecko(), 0)
-    setTimeout(() => processSystemInfo(), 0)
-    setTimeout(() => processServerTime(), 0)
-    setTimeout(() => processMemInfo(), 0)
-    setTimeout(() => processCPUData(), 0)
-    setTimeout(() => processCPUTemp(), 0)
-    setTimeout(() => processNetInfo(), 0)
-    setTimeout(() => processNetConnections(), 0)
-    setTimeout(() => processBlockchainInfo(), 0 )
-    setTimeout(() => processBlockSpeed(), 0 )
-    setTimeout(() => processNodeStatus(), 0)
-    setTimeout(() => processConsensus(), 0)
-    setTimeout(() => processUptime(), 0)
-    setTimeout(() => processDelegations(), 0)
-    setTimeout(() => processBlocks(), 0)
-}).catch( reason => {
-    //
-})
+        setTimeout(() => processCoingecko(), 0)
+        setTimeout(() => processSystemInfo(), 0)
+        setTimeout(() => processServerTime(), 0)
+        setTimeout(() => processMemInfo(), 0)
+        setTimeout(() => processCPUData(), 0)
+        setTimeout(() => processCPUTemp(), 0)
+        setTimeout(() => processNetInfo(), 0)
+        setTimeout(() => processNetConnections(), 0)
+        setTimeout(() => processBlockchainInfo(), 0 )
+        setTimeout(() => processBlockSpeed(), 0 )
+        setTimeout(() => processNodeStatus(), 0)
+        setTimeout(() => processConsensus(), 0)
+        setTimeout(() => processUptime(), 0)
+        setTimeout(() => processDelegations(), 0)
+        setTimeout(() => processBlocks(), 0)
+    })
+    .catch(r => {
+        console.info(`Config is wrong or not loaded!`)
+        console.info(r)
+    })
