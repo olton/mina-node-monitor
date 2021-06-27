7b2cdbe91e519d7b70c8f540b100ebdd6e3a76fb
Sergey Pimenov <sergey@pimenov.com.ua>
2021-06-27 17:59:31 +0300

    improved visual for price

--- client/js/price.js
+++ client/js/price.js
@@ -21,13 +21,13 @@ export const processPrice = async () => {
 
     if (data) {
         const mina = data[0]
-        const sign = mina.price_change_percentage_24h
-        const symbol = `<span class="ani-vertical mif-${sign === 0 ? '' : sign <= 0 ? 'arrow-down fg-red' : 'arrow-up fg-green'}"></span>`
         const price = (+mina.current_price).toFixed(2)
         const priceChange = +(mina.price_change_percentage_24h).toFixed(2)
         const priceDelta = (mina.price_change_24h).toFixed(2)
         const priceDeltaSign = priceDelta > 0 ? "+" : "";
-        const priceDeltaColor = priceDelta > 0 ? "fg-green" : "fg-red";
+        const priceDeltaColor = priceDelta == 0.00 ? "" : priceDelta > 0 ? "fg-green" : "fg-red";
+        const sign = mina.price_change_percentage_24h
+        const symbol = priceDelta == 0.00 ? `` : `<span class="ani-vertical mif-${sign < 0 ? 'arrow-down fg-red' : 'arrow-up fg-green'}"></span>`
 
         elCurrentPrice.html(`${price}`)
         elCurrency.html(currency.toUpperCase())
@@ -35,7 +35,7 @@ export const processPrice = async () => {
         elPriceHigh.html(mina.ath)
         elPriceLow.html(mina.atl)
 
-        elPriceArrow.html(`<span class="fg-normal ${priceDeltaColor}">${priceDeltaSign}${priceDelta}</span>${symbol}`)
+        elPriceArrow.html(`<span class="text-bold fg-accent ${priceDeltaColor}">${priceDeltaSign}${priceDelta}</span>${symbol}`)
 
         elBalance.text((globalThis.balance * globalThis.price).format(2, null, ",", ".") + " " + currency.toUpperCase())
 
