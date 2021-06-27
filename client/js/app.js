2ef3edd8f608c17e9259e79ed854cb20a1cfb98e
Sergey Pimenov <sergey@pimenov.com.ua>
2021-06-27 20:08:55 +0300

    switched to version 1.1.0

--- client/js/app.js
+++ client/js/app.js
@@ -10,7 +10,7 @@ import {processDelegations} from "./delegations"
 import {processBlocks} from "./explorer";
 import {processPrice} from "./price";
 
-const version = `1.0.5`
+const version = `1.1.0`
 
 $("#version").text(version)
 
