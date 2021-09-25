be6f0e9630ac4937cbf9ed23c41ca8bc81553760
Sergey Pimenov <sergey@pimenov.com.ua>
2021-09-25 21:19:36 +0300

    renamed coingecko.js to price.js

--- server/modules/price.js
+++ server/modules/price.js
@@ -1,6 +1,35 @@
 const fetch = require("node-fetch")
-const {TELEGRAM_BOT_URL, discord} = require("../helpers/messangers")
 const {parseTime, parseTelegramChatIDs} = require("../helpers/parsers")
+const {TELEGRAM_BOT_URL, discord} = require("../helpers/messangers");
+
+const getPriceInfo = async (currency = 'usd') => {
+    try {
+        const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=%CURRENCY%&ids=mina-protocol`.replace("%CURRENCY%", currency.toLowerCase())
+        const data = await fetch(url)
+
+        if (!data.ok) {
+            throw new Error("Error responding from price provider!")
+        }
+
+        return await data.json()
+    } catch (e) {
+        return null
+    }
+}
+
+const processPriceInfo = async () => {
+    const {currency, updateInterval} = globalThis.config.price
+    const _updateInterval = parseTime(updateInterval)
+
+    let data = await getPriceInfo(currency)
+
+    if (Array.isArray(data)) {
+        data[0].currency = currency
+        globalThis.cache.price = data
+    }
+
+    setTimeout(processPriceInfo, _updateInterval)
+}
 
 const processPriceSend = async () => {
     if (!globalThis.config) return
@@ -35,5 +64,6 @@ const processPriceSend = async () => {
 }
 
 module.exports = {
+    processPriceInfo,
     processPriceSend
 }
