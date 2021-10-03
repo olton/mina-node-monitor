e7017f67b96b5b87dc86b7583077332f65e32b38
Sergey Pimenov <sergey@pimenov.com.ua>
2021-10-03 10:08:39 +0300

    Client: improved start.js

--- client/start.js
+++ client/start.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-const {exec} = require("child_process")
+const {spawn} = require("child_process")
 
 const isset = (v, nullable = true) => {
     try {
@@ -16,11 +16,22 @@ const getArguments = () => {
     for (let i = 0; i < args.length; i++) {
         if (i % 2 !== 0) continue
         let key = ""+args[i]
-        let val = args[i + 1]
+        let _val, val = args[i + 1]
+
         if (key[0] === '-') {
             key  = key.substr(1)
         }
-        obj[key] = isNaN(val) ? val : +val
+
+        _val = isNaN(val) ? val : +val
+
+        if (isset(obj[key])) {
+            if (!Array.isArray(obj[key])) {
+                obj[key] = [obj[key]]
+            }
+            obj[key].push(_val)
+        } else {
+            obj[key] = _val
+        }
     }
 
     return obj
@@ -35,11 +46,25 @@ const updateConfigFromArguments = (c) => {
 
         let v = args[o]
 
-        if (!isNaN(v)) {
+        if (o === "nodes") {
+            let nodes = []
+            for(let n of v) {
+                n = n.split(":")
+                nodes.push({
+                    "name": n[0],
+                    "host": `${n[1]}:${n[2]}`,
+                    "https": !isset(n[3]) ? false : ""+n[3] === "true"
+                })
+            }
+            v = nodes
+        }
+        else if (!isNaN(v)) {
             v = Number(v)
-        } else if (['true', 'false'].includes(v.toLowerCase())) {
+        }
+        else if (typeof v === "string" && ['true', 'false'].includes(v.toLowerCase())) {
             v = v.toLowerCase() === 'true'
-        } else if (v.includes("[") || v.includes(",")) {
+        }
+        else if (typeof v === "string" && (v.includes("[") || v.includes(","))) {
             v = v.replace(/[\[\]"'`]/gi, "").split(",")
         }
 
@@ -65,27 +90,27 @@ const start = () => {
 
         let configFile, configJSON, configFileName
 
-        configFileName = fs.existsSync("./config.json") ? "./config.json" : "./config.example.json"
+        configFileName = fs.existsSync(__dirname+"/config.json") ? __dirname+"/config.json" : __dirname+"/config.example.json"
         configFile = fs.readFileSync(configFileName, 'utf-8')
         configJSON = updateConfigFromArguments(JSON.parse(configFile))
 
-        fs.writeFileSync('./config.json', JSON.stringify(configJSON, null, 4), {
+        fs.writeFileSync(__dirname+'/config.json', JSON.stringify(configJSON, null, 4), {
             encoding: "utf-8",
             flag: "w"
         })
 
-        if (fs.existsSync("./config.json") && !args.includes("--no-start")) {
-            exec("npm run client", (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
-                console.error(`stderr: ${stderr}`);
+        if (fs.existsSync(__dirname+"/config.json") && !args.includes("--no-start")) {
+            const clientProc = spawn("npm", ["run", "client"], {shell: true})
+            clientProc.stdout.on('data', data => {
+                console.log(`${data}`);
+            })
+            clientProc.stderr.on('data', data => {
+                console.error(`stderr:\n${data}`);
             })
         }
     } catch (e) {
         console.log(e.message)
+        console.log(e.stack)
     }
 }
 
