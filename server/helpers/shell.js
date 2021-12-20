ecf74ebd826b81deba2a533616e2b7f7e7dcd52d
Sergey Pimenov <sergey@pimenov.com.ua>
2021-12-20 17:04:39 +0200

    Server: added support for static files for webserver, Server: added config parameter `webRoot`. With this parameter you can set a webRoot folder other than default

--- server/helpers/shell.js
+++ server/helpers/shell.js
@@ -3,7 +3,7 @@ const fs = require("child_process")
 const processGetMinaVersion = () => {
     try {
         let version = fs.execSync('mina version')
-        return version.split(" ")[1]
+        return version.toString().split(" ")[1]
     } catch (e) {
         return "unknown"
     }
