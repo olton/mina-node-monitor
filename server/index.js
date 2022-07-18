59cb1ddaca378fd1c79c3026611115ce8cb1aa9a
Serhii Pimenov <serhii@pimenov.com.ua>
2022-07-18 10:26:08 +0300

    fix Vulnerability for doesn't sanitaze url from '..'. This code fully removed

--- server/index.js
+++ server/index.js
@@ -75,62 +75,15 @@ let server, useHttps = config.https && (config.https.cert && config.https.key)
 globalThis.isHttps = Boolean(useHttps)
 
 const requestListener = async (req, res) => {
-    const {webRoot} = config
+    res.setHeader("Content-Type", "text/html")
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.writeHead(200)
 
-    if (req.url === '/') {
-        res.setHeader("Content-Type", "text/html")
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        res.writeHead(200)
-
-        res.end(
-            welcomeHtml
-                .replace("%VER%", version)
-                .replace("%MINA%", processGetMinaVersion())
-        )
-
-    } else {
-        let filePath = webRoot ? webRoot : __dirname + req.url
-
-        const extname = String(path.extname(filePath)).toLowerCase();
-        const mimeTypes = {
-            '.html': 'text/html',
-            '.js': 'text/javascript',
-            '.css': 'text/css',
-            '.json': 'application/json',
-            '.png': 'image/png',
-            '.jpg': 'image/jpg',
-            '.gif': 'image/gif',
-            '.svg': 'image/svg+xml',
-            '.wav': 'audio/wav',
-            '.mp4': 'video/mp4',
-            '.woff': 'application/font-woff',
-            '.ttf': 'application/font-ttf',
-            '.eot': 'application/vnd.ms-fontobject',
-            '.otf': 'application/font-otf',
-            '.wasm': 'application/wasm'
-        };
-
-        let contentType = mimeTypes[extname] || 'application/octet-stream';
-
-        fs.readFile(filePath, function(error, content) {
-            if (error) {
-                if(error.code === 'ENOENT') {
-                    fs.readFile('./404.html', function(error, content) {
-                        res.writeHead(404, { 'Content-Type': 'text/html' });
-                        res.end(content, 'utf-8');
-                    });
-                }
-                else {
-                    res.writeHead(500);
-                    res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                }
-            }
-            else {
-                res.writeHead(200, { 'Content-Type': contentType });
-                res.end(content, 'utf-8');
-            }
-        });
-    }
+    res.end(
+        welcomeHtml
+            .replace("%VER%", version)
+            .replace("%MINA%", processGetMinaVersion())
+    )
 }
 
 if (useHttps) {
